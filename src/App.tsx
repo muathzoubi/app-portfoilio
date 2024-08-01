aab39d3e8f49fa78e93b259f4b4874338ddc4987
muathzoubi <moathaza@gmail.com>
2024-08-01 16:26:23 +0300

    sdcx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Card from './card';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const arr = [1, 2, 3, 4, 5, 6];
   const listdemos = [
     { title: 'Speech app', src: './1.jpeg' },
     { title: 'Char app', src: './2.jpeg' },
