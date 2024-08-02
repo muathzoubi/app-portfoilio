66e07c369ce36e2cd42864438a7b9917441ab584
muathzoubi <moathaza@gmail.com>
2024-08-02 04:41:33 +0300

    zz

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import React from 'react';
 import './App.css';
 import Card from './card';
 
 function App() {
+  
   const listdemos = [
     { title: 'Speech app', src: './1.jpeg' },
-    { title: 'Char app', src: './2.jpeg' },
-    { title: 'Speech app', src: './1.jpeg' },
-    { title: 'Speech app', src: './1.jpeg' },
-    { title: 'Speech app', src: './1.jpeg' },
+    { title: 'Chat app', src: './2.jpeg' },
+    { title: 'Speech 2 text app', src: './3.jpeg' },
+    { title: 'Read Text app', src: './4.jpeg' },
+    { title: 'log in form', src: './4.jpeg' },
+    { title: 'Draw App', src: './3.jpeg' },
+    { title: 'Editor App', src: './2.jpeg' },
+    { title: 'Images View App', src: './1.jpeg' },
   ];
   return (
     <>
       <h2>Passionate About Technology</h2>
       <p className="read-the-docs ">
-        Start discovering some of <code>@Muath A Alzoubi </code>works
+        Start discovering some of <code><a href='#'> @Muath A Alzoubi</a> </code>works
       </p>
+
       <div className="grid-container">
+      
         {listdemos.map((i) => (
+         
           <Card key={i.title} title={i.title} imgSrc={i.src} />
+        
         ))}
       </div>
     </>
